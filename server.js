a21f492db9424c57e3c911aeb2b5863b6af26ff6
generalustelaru <adrianpope@protonmail.com>
2024-04-19 09:24:02 +0300

    refactoring

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { WebSocketServer } from 'ws';
 const app = express();
-const port = 3000;
+const httpPort = 3000;
+const wsPort = 8080;
 
 app.use(express.static('public'));
 
@@ -24,11 +25,11 @@ app.get('/', (res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(port, () => {
-    console.info(`Server running at http://localhost:${port}`);
+app.listen(httpPort, () => {
+    console.info(`Server running at http://localhost:${httpPort}`);
 });
 
-const socketServer = new WebSocketServer({ port: 8080 });
+const socketServer = new WebSocketServer({ port: wsPort });
 
 socketServer.on('connection', function connection(ws) {
 
@@ -37,16 +38,16 @@ socketServer.on('connection', function connection(ws) {
             const messageObject = JSON.parse(message);
             console.log('received: %s: %s', messageObject.action, messageObject.details);
 
-            if (messageObject.action === 'refresh') {
+            if (messageObject.action == 'refresh') {
                 ws.send(JSON.stringify(state));
             }
 
-            if (messageObject.action === 'move') {
-                const allowed = moveRules.find(rule => rule.from === state.locationHex).allowed;
+            if (messageObject.action == 'move') {
+                const allowed = moveRules.find(rule => rule.from == state.locationHex).allowed;
 
                 if (allowed.includes(messageObject.details)) {
                     state.locationHex = messageObject.details;
-                    state.allowedMoves = moveRules.find(rule => rule.from === messageObject.details).allowed;
+                    state.allowedMoves = moveRules.find(rule => rule.from == messageObject.details).allowed;
 
                     ws.send(JSON.stringify(state));
                 } else {
