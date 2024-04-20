e4e703a8481dee2a6feb0c1af3c3c35dbba53228
generalustelaru <adrianpope@protonmail.com>
2024-04-20 13:16:56 +0300

    extracted maphex to class file

---
+++ elements/MapHex.js
@@ -0,0 +1,19 @@
+
+export class MapHex
+{
+    constructor(width, name, x, y, fill){
+
+        return new Konva.RegularPolygon({
+            x: width / 2,
+            y: width / 2,
+            offsetX: x,
+            offsetY: y,
+            sides: 6,
+            radius: 100,
+            fill: fill,
+            stroke: 'black',
+            strokeWidth: 1,
+            id: name,
+        });
+    }
+}
