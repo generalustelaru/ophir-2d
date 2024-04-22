666d86c57e5f394530b7e1836477728a608c385c
generalustelaru <adrianpope@protonmail.com>
2024-04-22 19:27:05 +0300

    fixed webpack mode warning

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const __dirname = path.dirname(__filename);
 
 export default {
     entry: './main.js',
+    mode: 'development',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public'),
