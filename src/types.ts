da5ecfde96cad971e66c81e90329ec59c6dc75a7
generalustelaru <adrianpope@protonmail.com>
2024-04-29 07:14:17 +0300

    better typing in state.ts

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,33 @@
 
+import Konva from 'konva';
+
 export type PlayerId = "playerWhite" | "playerYellow" | "playerRed" | "playerGreen";
 
+export type Location =
+    | 'center' | 'topRight' | 'right' | 'bottomRight'
+    | 'bottomLeft' | 'left' | 'topLeft';
+
+export type Status = "empty" | "lobby" | "full" | "started";
+
+export type PlayerState = {
+    location: Location,
+    allowedMoves: Location[],
+}
+
 export type ServerState = {
-    status: "empty" | "lobby" | "full" | "started",
+    status: Status,
     sessionOwner: PlayerId | null,
     availableSlots: PlayerId[],
-    players: any,
-};
+    players: Record<PlayerId, PlayerState> | {},
+}
+
 export type State = {
     playerId: string | null,
     isBoardDrawn: boolean,
     server: ServerState,
-    map: any,
+    map: {
+        playerShip: Konva.Rect | null,
+        opponentShips: Konva.Rect[],
+        islands: Konva.RegularPolygon[],
+    },
 }
