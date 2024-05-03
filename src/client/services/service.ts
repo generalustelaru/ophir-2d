298fe8b8d4b84048090c9cb66fefba41db578f2e
Adrian Pop <adrian.pop@qubiz.com>
2024-05-03 21:28:50 +0300

    expanded the use of types

--- src/client/services/service.ts
+++ src/client/services/service.ts
@@ -1,31 +1,30 @@
-import { EventPayload } from "../../shared_types";
+import { EventPayload, CustomEventTitle } from "../../shared_types";
 
 export interface ServiceStaticInterface {
     new (): ServiceInterface;
     getInstance(): ServiceInterface;
 }
 export interface ServiceInterface {
-    broadcastEvent: (event: string, payload: EventPayload) => void,
+    broadcastEvent: (event: CustomEventTitle, payload: EventPayload) => void,
 }
 export class Service implements ServiceInterface {
     static instance: Service | null = null;
 
-    broadcastEvent = (eventType: string, detail: EventPayload = null) => {
+    broadcastEvent = (eventType: CustomEventTitle, detail: EventPayload = null) => {
         window.dispatchEvent(
             new CustomEvent(
                 eventType,
-                {detail: detail}
+                { detail: detail }
             )
         );
     }
 
-    static getInstance<T extends ServiceInterface>(): T {
+    static getInstance<I extends ServiceInterface>(): I {
 
         if(!this.instance) {
             this.instance = new this();
-            console.log(`Instance of ${this.name} created`);
         }
 
-        return this.instance as T;
+        return this.instance as I;
     }
 }
