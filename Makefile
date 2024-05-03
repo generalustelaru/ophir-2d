1695ab81d6617b387279af48d9edd512224f90a3
generalustelaru <adrianpope@protonmail.com>
2024-05-03 12:34:29 +0300

    prepped webpack for dual configuration

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
-build:
-	npm run build
+app:
+	npm run build_client
+
+end:
+	# npm run build_server
+
+all:
+	npm run build_client
+	# npm run build_server
+	node server.js
 
 serve:
 	node server.js
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,30 +2,35 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-    entry: {
-        'client': './src/client/main.ts',
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [new TerserPlugin()],
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'client.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.json'],
-    },
-    // devtool: 'eval-source-map', // for development debugging
-    mode: 'none',
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-        }]
-    },
+module.exports = (env, argv) => {
+    const label = env.isServer === 'true' ? 'server' : 'client';
+    console.info(`Bundling ${label} code...`);
+
+    return {
+        entry: {
+            [label]: `./src/${label}/main.ts`,
+        },
+        optimization: {
+            minimize: true,
+            minimizer: [new TerserPlugin()],
+        },
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: '[name].js',
+        },
+        resolve: {
+            extensions: ['.ts', '.js', '.json'],
+        },
+        // devtool: 'eval-source-map', // for development debugging
+        mode: 'none',
+        module: {
+            rules: [{
+                test: /\.ts$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            }]
+        },
+    }
 };
 
 // TODO: How do I split configuration between client and server?
