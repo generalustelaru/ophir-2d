bdff3147ca046b847777e81d5545a9aea8a3d65a
generalustelaru <adrianpope@protonmail.com>
2024-11-01 13:01:17 +0200

    added color-coded cargo hold

--- src/client/canvas_objects/cargoHold.ts
+++ src/client/canvas_objects/cargoHold.ts
@@ -1,1 +1,43 @@
-// TODO: resume from here
+
+import Konva from 'konva';
+import { HexaColor, MapHexInterface, IslandData, SettlementData } from '../client_types';
+import { Vector2d } from 'konva/lib/types';
+import { LocationToken } from './locationToken';
+import clientConstants from '../client_constants';
+
+const { CARGO_HOLD_DATA, COLOR } = clientConstants;
+
+export class CargoHold {
+
+    group: Konva.Group;
+    hold: Konva.Rect;
+    constructor(
+        color: HexaColor,
+        isLargeHold: boolean = false,
+    ) {
+        this.group = new Konva.Group({
+            width: 200,
+            height: 200,
+            x: 525,
+            y: 25,
+        });
+
+        this.hold = new Konva.Rect({
+            width: 200,
+            height: isLargeHold ? 200 : 100,
+            fill: color,
+            cornerRadius: 15,
+            strokeWidth: 1,
+        });
+
+        this.group.add(this.hold);
+    }
+
+    public getElement() {
+        return this.group;
+    }
+
+    public upgradeHold() {
+        this.hold.height(200);
+    }
+}
--- src/client/client_types.ts
+++ src/client/client_types.ts
@@ -1,10 +1,11 @@
 import { Vector2d } from 'konva/lib/types';
-import { HexId, PlayerId, SharedState, Coordinates, Action, ActionDetails, SettlementId } from '../shared_types';
+import { HexId, PlayerId, SharedState, Coordinates, Action, ActionDetails, SettlementId, CargoItem } from '../shared_types';
 import Konva from 'konva';
 
 export type HexaColor = `#${string}`;
 export type HexOffset = { id: HexId, x: number, y: number };
 export type SettlementData = { shape: string, fill: HexaColor };
+export type CargoHoldData = { shape: string, fill: HexaColor };
 export type IslandData = { x: number , y: number, shape: string };
 export type CustomEventTitle = "connected" | "action" | "update" | "error" | "info";
 
@@ -32,6 +33,7 @@ export type ClientConstants = {
     ISLAND_DATA: Record<HexId, (IslandData)>,
     SETTLEMENT_DATA: Record<SettlementId, SettlementData>,
     SHIP_DATA: { shape: string },
+    CARGO_HOLD_DATA: Record<CargoItem, CargoHoldData>,
     EVENT: Record<CustomEventTitle, CustomEventTitle>, // TODO: cull constants that are replaceable by types
 }
 
