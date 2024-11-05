c1d4855dcf9e04e5e80dd9ea360ee35c79406f54
generalustelaru <adrianpope@protonmail.com>
2024-11-05 16:44:29 +0200

    cleaned up initializations and entry point on client

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,14 +1,70 @@
-import sharedConstants from '../shared_constants';
-import { CommunicationService, CommunicationInterface } from './services/CommService';
-import { CanvasService, CanvasInterface } from './services/CanvasService';
-import { EventHandler } from './eventHandler';
+import { InfoEventPayload, ActionEventPayload, ErrorEventPayload } from "./client_types";
+import state from "./state";
+import { CommunicationService, CommunicationInterface } from "./services/CommService";
+import { CanvasService, CanvasInterface } from "./services/CanvasService";
+import { UserInterfaceService, UiInterface } from "./services/UiService";
+import sharedConstants from "../shared_constants";
+import clientConstants from "./client_constants";
+import { SharedState } from "../shared_types";
+const { ACTION, STATUS, CONNECTION } = sharedConstants;
+const { EVENT } = clientConstants;
 
-const { CONNECTION } = sharedConstants;
+// Initializations
+const commService: CommunicationInterface = CommunicationService.getInstance([CONNECTION.wsAddress]);
+const canvasService: CanvasInterface = CanvasService.getInstance([]);
+const uiService: UiInterface = UserInterfaceService.getInstance([]);
 
-new EventHandler(); // Enables inter-class communication
+//Send player action to server
+window.addEventListener(
+    EVENT.action as any,
+    (event) => {
+        const payload: ActionEventPayload = event.detail;
+        commService.sendMessage(
+            payload.action,
+            payload.details
+        )
+    },
+);
 
-const commService: CommunicationInterface = CommunicationService.getInstance();
-commService.createConnection(CONNECTION.wsAddress);
+//Display errors
+window.addEventListener(
+    EVENT.error as any,
+    (event) => {
+        const payload: ErrorEventPayload = event.detail;
+        console.error(payload.error);
+        alert(payload.error);
+    },
+);
 
-const canvasService: CanvasInterface = CanvasService.getInstance();
-canvasService.initiateCanvas();
+// Get server data on connection
+window.addEventListener(
+    EVENT.connected,
+    () => commService.sendMessage(ACTION.inquire),
+);
+
+// Update client on server state update
+window.addEventListener(EVENT.update, () => {
+    const serverState = state.server as SharedState;
+    if (serverState.gameStatus === STATUS.started) {
+        if (state.isBoardDrawn) {
+            canvasService.updateElements();
+        } else {
+            uiService.setInfo('The game has started');
+            canvasService.drawElements();
+            state.isBoardDrawn = true;
+        }
+        uiService.updateGameControls();
+    } else {
+        uiService.updateLobbyControls();
+    }
+});
+
+window.addEventListener(
+    EVENT.info as any,
+    (event) => {
+        const payload: InfoEventPayload = event.detail;
+        uiService.setInfo(payload.text)
+    }
+);
+
+commService.createConnection();
