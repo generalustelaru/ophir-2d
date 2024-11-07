50c9ac833535155bc5091b90d4cfd6085e4ecf29
generalustelaru <adrianpope@protonmail.com>
2024-11-07 19:33:42 +0200

    fixed player mats not appearing on spectator clients

--- src/client/services/PlayerSegmentPainter.ts
+++ src/client/services/PlayerSegmentPainter.ts
@@ -5,7 +5,6 @@ import { PlayMat } from '../canvas_objects/PlayMat';
 import  clientConstants from '../client_constants';
 import clientState from '../state';
 
-
 const { COLOR } = clientConstants;
 export class PlayerSegmentPainter extends Service implements CanvasSegmentInterface {
 
@@ -18,19 +17,19 @@ export class PlayerSegmentPainter extends Service implements CanvasSegmentInterf
 
     public drawElements(): void {
         const thisPlayerId = clientState.localPlayerId;
-        const activePlayer = clientState.received.players.find(player => player.isActive);
-
-        if (!thisPlayerId) {
-            throw new Error('Player ID is missing!');
-        }
-
+        // MARK: draw playmats
         const matOffsets = [20, 140, 260, 380];
-
-        clientState.received.players.forEach((player) => {
-            const isLocalPlayer = player.id === thisPlayerId;
-            const isPlayerActive = thisPlayerId === activePlayer?.id;
+        clientState.received.players.forEach(player => {
             const matOffset = matOffsets.shift() as number;
-            const playMat = new PlayMat(player.id, isLocalPlayer, isPlayerActive, COLOR[player.id], this.matchPlayerColor(COLOR[player.id]), matOffset);
+            const isLocalPlayer = player.id === thisPlayerId;
+
+            const playMat = new PlayMat(
+                player,
+                isLocalPlayer,
+                COLOR[player.id],
+                this.matchPlayerColor(COLOR[player.id]),
+                matOffset
+            );
             this.layer.add(playMat.getElement());
             clientState.konva.playMats.push(playMat);
         });
@@ -38,9 +37,8 @@ export class PlayerSegmentPainter extends Service implements CanvasSegmentInterf
 
     public updateElements(): void {
 
-        clientState.konva.playMats.forEach((playMat) => {
-            const player = clientState.received.players.find((player) => player.id === playMat.getId());
-
+        clientState.konva.playMats.forEach(playMat => {
+            const player = clientState.received.players.find(player => player.id === playMat.getId());
             if (player) {
                 playMat.updateElements(player);
             } else {
