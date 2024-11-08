4155b3a881a38a95ff8728dd7934aa391e677f10
generalustelaru <adrianpope@protonmail.com>
2024-11-08 15:59:23 +0200

    removed stray import

--- src/client/client_types.ts
+++ src/client/client_types.ts
@@ -2,7 +2,6 @@ import { Vector2d } from 'konva/lib/types';
 import { HexId, PlayerId, SharedState, Coordinates, Action, ActionDetails, SettlementId, ManifestItem, NewState, Player } from '../shared_types';
 import Konva from 'konva';
 import { ServiceInterface } from './services/Service';
-import { Group } from 'konva/lib/Group';
 
 export type Color = `#${string}`;
 export type HexOffset = { id: HexId, x: number, y: number };
