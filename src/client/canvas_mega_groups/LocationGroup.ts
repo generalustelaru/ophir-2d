a2b2b0bcf5587a5bc2af9c80fd8b73871e68c209
generalustelaru <adrianpope@protonmail.com>
2024-11-11 16:16:42 +0200

    seeded locationGroup for future Market and Temple

--- src/client/canvas_mega_groups/LocationGroup.ts
+++ src/client/canvas_mega_groups/LocationGroup.ts
@@ -2,9 +2,9 @@ import { CanvasMegaGroupInterface } from "../client_types";
 
 export class LocationGroup implements CanvasMegaGroupInterface {
     drawElements(): void {
-        throw new Error("Method not implemented.");
+        console.warn('drawElements: Method not implemented.');
     }
     updateElements(): void {
-        throw new Error("Method not implemented.");
+        console.warn('updateElements: Method not implemented.');
     }
 }
