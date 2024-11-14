b52fefc681002a0101e8df040c551c641fcc10ca
generalustelaru <adrianpope@protonmail.com>
2024-11-14 13:08:12 +0200

    removed debug logs

--- src/server/services/GameSetupService.ts
+++ src/server/services/GameSetupService.ts
@@ -31,8 +31,6 @@ export class GameSetupService extends Service {
             privateState: privateState,
         }
 
-        console.log(bundle.sharedState.market);
-
         return bundle;
     };
 
