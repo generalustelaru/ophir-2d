d37cd86defcff272cad31506a6caea10ec159fe5
generalustelaru <adrianpope@protonmail.com>
2024-11-24 10:18:01 +0200

    added gold and metal icons on exchange placard

--- src/client/groups/location/ExchangePlacard.ts
+++ src/client/groups/location/ExchangePlacard.ts
@@ -3,7 +3,7 @@ import { DynamicGroupInterface, GroupLayoutData } from '../../client_types';
 import clientConstants from '../../client_constants';
 import { HexId } from '../../../shared_types';
 
-const { COLOR } = clientConstants;
+const { COLOR, CARGO_ITEM_DATA } = clientConstants;
 
 export class ExchangePlacard implements DynamicGroupInterface<HexId> {
 
@@ -32,9 +32,29 @@ export class ExchangePlacard implements DynamicGroupInterface<HexId> {
             cornerRadius: 15,
         });
 
-        this.group.add(
+        const goldIcon = new Konva.Path({
+            data: CARGO_ITEM_DATA.gold.shape,
+            fill: CARGO_ITEM_DATA.gold.fill,
+            x: 10,
+            y: 10,
+            scaleX: 3,
+            scaleY: 3,
+        });
+
+        const silverIcon = new Konva.Path({
+            data: CARGO_ITEM_DATA.silver.shape,
+            fill: CARGO_ITEM_DATA.silver.fill,
+            x: 80,
+            y: 10,
+            scaleX: 3,
+            scaleY: 3,
+        });
+
+        this.group.add(...[
             this.background,
-        );
+            goldIcon,
+            silverIcon,
+        ]);
     }
 
     public updateElement(playerLocation: HexId): void {
