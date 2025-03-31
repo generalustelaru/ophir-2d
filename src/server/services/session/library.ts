07eb31d938c1796d4b311875397e12f1ee036d08
generalustelaru <adrianpope@protonmail.com>
2025-03-31 14:52:32 +0300

    Session Refactoring: - Extracted session utility functions to library file.

---
+++ src/server/services/session/library.ts
@@ -0,0 +1,38 @@
+import { ErrorResponse } from "../../../shared_types";
+
+export type Probable<T> = { err: true, message: string } | { err?: false, data: T };
+
+function pass<T>(data: T): Probable<T> {
+    return { data }
+}
+
+function fail(message: string): Probable<any> {
+    return { err: true, message }
+}
+
+function checkConditions(arr: Array<boolean>): Probable<true> {
+    if (arr.includes(false))
+        return fail('');
+    return pass(true);
+}
+
+function validationErrorResponse(){
+    return issueErrorResponse('Malformed request.');
+}
+
+function issueErrorResponse(message: string, params?: object): ErrorResponse {
+    const error = `ERROR: ${message}`;
+    console.error(error, params);
+
+    return { error };
+}
+
+const lib = {
+    pass,
+    fail,
+    checkConditions,
+    validationErrorResponse,
+    issueErrorResponse,
+}
+
+export default lib;
