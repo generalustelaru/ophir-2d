9f93462d5cdf39ca08752d365003fae1b07e66a2
generalustelaru <adrianpope@protonmail.com>
2025-07-11 12:40:48 +0300

    Detached the vp dial into a reusable class.

---
+++ src/client/groups/VictoryPointDial.ts
@@ -0,0 +1,73 @@
+import Konva from "konva";
+import { DynamicGroupInterface } from "../client_types";
+import { Coordinates } from "../../shared_types";
+import clientConstants from "../client_constants";
+
+const { COLOR, ICON_DATA } = clientConstants;
+export class VictoryPointDial implements DynamicGroupInterface<number> {
+
+    private group: Konva.Group;
+    private vp: Konva.Text;
+    constructor(
+        position: Coordinates,
+        value?: number,
+    ) {
+        this.group = new Konva.Group({
+            x: position.x, y: position.y, width: 66, height: 96
+        });
+
+        const disc = new Konva.Circle({
+            x: this.group.width() / 2,
+            y: this.group.height() / 2 + 15,
+            radius: 26,
+            fill: COLOR.vpGold,
+        });
+
+        const wreathY = this.group.height() / 2 - 6;
+        const wreathX = 6;
+        const leftWreath = new Konva.Path({
+            data: ICON_DATA.half_wreath.shape,
+            fill: ICON_DATA.half_wreath.fill,
+            x: wreathX,
+            y: wreathY,
+            scale: { x: 2, y: 2 },
+        });
+
+        const rightWreath = new Konva.Path({
+            data: ICON_DATA.half_wreath.shape,
+            fill: ICON_DATA.half_wreath.fill,
+            x: wreathX + 27 * 2,
+            y: wreathY,
+            scale: { x: -2, y: 2 },
+        });
+
+        const backdrift = value && value > 9 ? 20 : 9
+        this.vp = new Konva.Text({
+            x: this.group.width() / 2 - backdrift,
+            y: this.group.height() / 2 - 3,
+            text: value ? String(value) : '0',
+            fontSize: 40,
+            fill: COLOR.boneWhite,
+            stroke: 'black',
+            strokeWidth: 2,
+            fontFamily: 'Calibri',
+        });
+
+        this.group.add(...[
+            disc,
+            leftWreath,
+            rightWreath,
+            this.vp,
+        ]);
+    }
+
+    public update(value: number) {
+        const backdrift = value > 9 ? 20 : 9;
+        this.vp.x(this.group.width() / 2 - backdrift)
+        this.vp.text(String(value));
+    }
+
+    public getElement(): Konva.Group {
+        return this.group;
+    }
+}
