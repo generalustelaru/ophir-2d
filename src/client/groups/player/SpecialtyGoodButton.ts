8bb6ac63bc8f4804a17fdef3d3cea01ccff39ca9
generalustelaru <adrianpope@protonmail.com>
2025-07-14 09:40:46 +0300

    Added non-functional specialty good button.

---
+++ src/client/groups/player/SpecialtyGoodButton.ts
@@ -0,0 +1,53 @@
+import Konva from 'konva';
+import { DynamicGroupInterface } from '../../client_types';
+import { ActionButton } from '../ActionButton';
+import { Player, Coordinates, ClientMessage, Action, LaconicAction } from '../../../shared_types';
+import clientConstants from '../../client_constants';
+
+const { CARGO_ITEM_DATA, COLOR } = clientConstants;
+const size = { width: 50, height: 50 };
+
+export class SpecialtyGoodButton extends ActionButton implements DynamicGroupInterface<boolean> {
+
+    constructor(
+        stage: Konva.Stage,
+        // message: ClientMessage | null,
+        player: Player,
+        position: Coordinates,
+    ) {
+        const layout = { ...position, ...size };
+        const specialty = player.specialist.specialty;
+        // const message = specialty ? { action: Action.inquire, payload: null } : null;
+
+        super(stage, layout, null);
+
+        const background = new Konva.Rect({
+            ...size,
+            fill: COLOR[`holdDark${player.color}`],
+            cornerRadius: 10,
+        });
+        this.group.add(background);
+
+        if (specialty) {
+            const iconData = CARGO_ITEM_DATA[specialty];
+            const tradeGoodIcon = new Konva.Path({
+                data: iconData.shape,
+                fill: iconData.fill,
+                stroke: 'white',
+                strokeWidth: .75,
+                scale: { x: 3, y: 3 },
+                x: 7,
+                y: 7,
+            })
+            this.group.add(tradeGoodIcon);
+        }
+    }
+
+    public getElement(): Konva.Group {
+        return this.group;
+    }
+
+    public update(maySell: boolean): void {
+        console.log({maySell})
+    }
+}
