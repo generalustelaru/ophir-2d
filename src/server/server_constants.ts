174cf857b83048f13563a676e6081bd576e8dd05
generalustelaru <adrianpope@protonmail.com>
2025-07-24 09:43:23 +0300

    re-enabled implemented specialists

--- src/server/server_constants.ts
+++ src/server/server_constants.ts
@@ -11,13 +11,13 @@ const serverConstants: ServerConstants = {
         //     specialty: 'linen',
         //     description: 'May complete temple trades using any market slot.',
         // },
-        // {
-        //     name: SpecialistName.ambassador,
-        //     displayName: 'Ambassador',
-        //     startingFavor: 0,
-        //     specialty: null,
-        //     description: 'Begins with fully upgraded cargo hold.',
-        // },
+        {
+            name: SpecialistName.ambassador,
+            displayName: 'Ambassador',
+            startingFavor: 0,
+            specialty: null,
+            description: 'Begins with fully upgraded cargo hold.',
+        },
         // {
         //     name: SpecialistName.chancellor,
         //     displayName: 'Chancellor',
@@ -53,13 +53,13 @@ const serverConstants: ServerConstants = {
             specialty: 'marble',
             description: 'Gains Favor after voluntarily ending the turn at the Temple.',
         },
-        // {
-        //     name: SpecialistName.temple_guard,
-        //     displayName: 'Temple Guard',
-        //     startingFavor: 2,
-        //     specialty: 'ebony',
-        //     description: 'Adds 1 to influence checks (up to 6).',
-        // },
+        {
+            name: SpecialistName.temple_guard,
+            displayName: 'Temple Guard',
+            startingFavor: 2,
+            specialty: 'ebony',
+            description: 'Adds 1 to influence checks (up to 6).',
+        },
         {
             name: SpecialistName.postmaster,
             displayName: 'Postmaster',
