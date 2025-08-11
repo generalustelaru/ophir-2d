7a990b513b5a258b3c4b0c3d594b3fe49dc299bd
generalustelaru <adrianpope@protonmail.com>
2025-08-11 08:24:43 +0300

    modified environment variable handling

--- src/server/configuration.ts
+++ src/server/configuration.ts
@@ -5,11 +5,18 @@ export const HTTP_PORT = String(process.env.HTTP_PORT);
 export const WS_PORT = Number(process.env.WS_PORT);
 export const SERVER_NAME = String(process.env.SERVER_NAME);
 export const SHUTDOWN_AUTH = String(process.env.SHUTDOWN_AUTH);
-export const IDLE_CHECKS = Boolean(Number(process.env.IDLE_CHECKS));
+export const IDLE_CHECKS = Boolean(process.env.IDLE_CHECKS === 'true');
 export const IDLE_TIMEOUT = (60 * 1000) * Number(process.env.IDLE_TIMEOUT);
-export const RICH_PLAYERS = Boolean(Number(process.env.RICH_PLAYERS));
+export const RICH_PLAYERS = Boolean(process.env.RICH_PLAYERS === 'true');
 export const CARGO_BONUS = Number(process.env.CARGO_BONUS);
-export const SINGLE_PLAYER = Boolean(Number(process.env.SINGLE_PLAYER));
-export const SHORT_GAME = Boolean(Number(process.env.SHORT_GAME));
-export const INCLUDE = JSON.parse(String(process.env.INCLUDE));
-export const PERSIST_SESSION = Boolean(Number(process.env.PERSIST_SESSION)); //TODO: remove persistence logic after implementing DB solution
+export const SINGLE_PLAYER = Boolean(process.env.SINGLE_PLAYER === 'true');
+export const SHORT_GAME = Boolean(process.env.SHORT_GAME === 'true');
+export const INCLUDE = (() => {
+    try {
+        return  JSON.parse(String(process.env.INCLUDE));
+    } catch (error) {
+        console.error('Could not parse INCLUDE variable!');
+        return [];
+    }
+})();
+export const PERSIST_SESSION = Boolean(process.env.PERSIST_SESSION === 'true'); //TODO: remove persistence logic after implementing DB solution
