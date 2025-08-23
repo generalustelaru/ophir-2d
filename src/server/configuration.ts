6a5c33d0b7a58dce9ec3c950dd9a308fbcafabe4
generalustelaru <adrianpope@protonmail.com>
2025-08-23 09:04:19 +0300

    Added new debug option

--- src/server/configuration.ts
+++ src/server/configuration.ts
@@ -8,6 +8,7 @@ export const SHUTDOWN_AUTH = String(process.env.SHUTDOWN_AUTH);
 export const IDLE_CHECKS = Boolean(process.env.IDLE_CHECKS === 'true');
 export const IDLE_TIMEOUT = (60 * 1000) * Number(process.env.IDLE_TIMEOUT);
 export const RICH_PLAYERS = Boolean(process.env.RICH_PLAYERS === 'true');
+export const FAVORED_PLAYERS = Boolean(process.env.FAVORED_PLAYERS === 'true');
 export const CARGO_BONUS = Number(process.env.CARGO_BONUS);
 export const SINGLE_PLAYER = Boolean(process.env.SINGLE_PLAYER === 'true');
 export const SHORT_GAME = Boolean(process.env.SHORT_GAME === 'true');
