05a8d25817395b8889221290b2253c04d6da6a77
generalustelaru <adrianpope@protonmail.com>
2025-08-30 20:25:59 +0300

    sell goods UI WIP

--- src/client/groups/modals/SellGodsModal.ts
+++ src/client/groups/modals/SellGodsModal.ts
@@ -1,18 +1,12 @@
 import Konva from "konva";
 import { ModalBase } from "./ModalBase";
-import { Action, ItemName, MarketFluctuations, MarketOffer, MarketSlotKey, PlayState } from "~/shared_types";
+import { Action, Coordinates, ItemName, MarketFluctuations, MarketOffer, MarketSlotKey, PlayState, TradeGood } from "~/shared_types";
 import { MarketCard, CoinDial } from "../GroupList";
 import {GroupLayoutData, ModalInterface } from "~/client_types";
 import clientConstants from "~/client_constants";
 
 const { CARGO_ITEM_DATA } = clientConstants;
 
-const cargoDisplayLayout = [
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-]
 
 export class SellGoodsModal extends ModalBase implements ModalInterface<PlayState, MarketSlotKey>{
     private marketCardLayout: GroupLayoutData;
@@ -21,6 +15,7 @@ export class SellGoodsModal extends ModalBase implements ModalInterface<PlayStat
     private market: MarketOffer | null = null;
     private marketCard: MarketCard | null = null;
     private coinDial: CoinDial;
+    private cargoItemLayout: Array<Coordinates>;
 
     constructor(stage: Konva.Stage) {
         super(stage);
@@ -38,6 +33,19 @@ export class SellGoodsModal extends ModalBase implements ModalInterface<PlayStat
             },
             0,
         );
+        const driftX = this.background.x();
+        const driftY = this.background.y();
+        this.cargoItemLayout = [
+            { x: 0, y: 0 },
+            { x: 20, y: 0 },
+            { x: 40, y: 0 },
+            { x: 60, y: 0 },
+        ].map(c => {
+            return {
+                x: driftX + c.x,
+                y: driftY + c.y,
+            }
+        });
 
         this.group.add(this.coinDial.getElement())
     }
@@ -46,21 +54,41 @@ export class SellGoodsModal extends ModalBase implements ModalInterface<PlayStat
         this.market = state.market
         this.fluctuations = state.setup.marketFluctuations;
         this.playerCargo = state.players.find(p => p.isActive)?.cargo || [];
+        console.log('UPDATED');
+        console.log({cargo: this.playerCargo})
     }
 
     public show(slot: MarketSlotKey) {
         if (!this.market || !this.fluctuations)
             throw new Error("Cannot render modal! Update data is missing.");
+        console.log('SHOW')
+        console.log({cargo: this.playerCargo});
+        // this.marketCard?.getElement().destroy();
+        // this.marketCard = new MarketCard(
+        //     this.stage,
+        //     this.marketCardLayout,
+        //     null,
+        //     this.market[slot],
+        //     this.fluctuations[slot],
+        // );
+        const goodTypes: Array<ItemName> = ['gems', 'ebony', 'marble', 'linen'];
+        const playerGoods = this.playerCargo.filter(i => goodTypes.includes(i));
+        playerGoods.forEach((good, index) => {
+            const data = CARGO_ITEM_DATA[good];
+            const coords = this.cargoItemLayout[index];
+            const icon = new Konva.Path({
+                x: coords.x,
+                y: coords.y,
+                data: data.shape,
+                fill: data.fill,
+                stroke: 'white',
+                strokeWidth: 1,
+                scale: { x: 2, y: 2 },
+            });
+            this.group.add(icon);
+        })
 
-        this.marketCard?.getElement().destroy();
-        this.marketCard = new MarketCard(
-            this.stage,
-            this.marketCardLayout,
-            null,
-            this.market[slot],
-            this.fluctuations[slot],
-        )
-        this.group.add(this.marketCard.getElement());
+        // this.group.add(this.marketCard.getElement());
 
         const coinValue = this.market[slot].reward.coins  + this.fluctuations[slot];
         this.coinDial.update(coinValue);
