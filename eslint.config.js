6e354b7abe21e80ac5f44afb27b03ceb6c285a16
generalustelaru <adrianpope@protonmail.com>
2025-10-16 17:15:05 +0300

    Fixed formatting and removed redundant property in ESLint config.

--- eslint.config.js
+++ eslint.config.js
@@ -2,41 +2,40 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 
 export default [
-{
-    ignores: ['public/**', '**/*_constants.ts', '*.config.js'],
-},
-{
-    languageOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        parser: tsParser,
-        globals: {
-            // ES2021 globals
+    {
+        ignores: ['public/**', '**/*_constants.ts'],
+    },
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parser: tsParser,
+            globals: {
+                // ES2021 globals
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            'max-len': ['warn', { code: 125 }],
+            'object-curly-spacing': ['error', 'always'],
+            'array-bracket-spacing': ['error', 'never'],
+            'comma-dangle': ['error', 'always-multiline'],
+            'indent': ['error', 4, { 'SwitchCase': 1 }],
         },
     },
-    plugins: {
-        '@typescript-eslint': tsPlugin,
+    {
+        files: ['src/client/**/*.ts'],
+        languageOptions: {
+            globals: { window: 'readonly', document: 'readonly' },
+        },
     },
-    rules: {
-        'indent': ['error', 4],
-        'quotes': ['error', 'single'],
-        'semi': ['error', 'always'],
-        'max-len': ['warn', { code: 125 }],
-        'object-curly-spacing': ['error', 'always'],
-        'array-bracket-spacing': ['error', 'never'],
-        'comma-dangle': ['error', 'always-multiline'],
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
-    },
-},
-{
-    files: ['src/client/**/*.ts'],
-    languageOptions: {
-        globals: { window: 'readonly', document: 'readonly' },
-    },
-},
-{
-    files: ['src/server/**/*.ts'],
-    languageOptions: {
-        globals: { process: 'readonly', __dirname: 'readonly' },
-    },
-}];
+    {
+        files: ['src/server/**/*.ts'],
+        languageOptions: {
+            globals: { process: 'readonly', __dirname: 'readonly' },
+        },
+    }];
