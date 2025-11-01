a63b19a19e847c3a6b63d385e3dd8f40e273560c
generalustelaru <adrianpope@protonmail.com>
2025-11-01 13:13:38 +0200

    Refined force turn modal

--- src/client/groups/modals/ForceTurnModal.ts
+++ src/client/groups/modals/ForceTurnModal.ts
@@ -1,26 +1,38 @@
 import Konva from 'konva';
 import { ModalBase } from './ModalBase';
+import constants from '~/client_constants';
+
+const { ICON_DATA, COLOR } = constants;
 
 export class ForceTurnModal extends ModalBase {
     constructor(stage: Konva.Stage) {
         super(
             stage,
             { hasSubmit: false, cancelLabel: 'Close' },
-            { width: 400, height: 150 },
+            { width: 380, height: 160 },
         );
 
         const text = new Konva.Text({
-            text: 'You\'ve failed your second sailing attempt.\nThe turn has passed to the next player.',
+            text: 'Your second move failed the influence check.\nThe turn has passed to the next player.',
             fill: 'white',
             fontSize: 18,
             width: this.contentGroup.width(),
             height: this.contentGroup.height(),
             align: 'center',
-            verticalAlign: 'left',
+            verticalAlign: 'top',
+            y: 10,
             fontFamily: 'Custom',
         });
+        const anchorPath = ICON_DATA.anchored;
+        const disabledAnchor = new Konva.Path({
+            data: anchorPath.shape,
+            fill: COLOR.disabled,
+            scale: { x: 1.5, y: 1.5 },
+            x: this.contentGroup.width() / 2 - 24,
+            y: 50,
+        });
 
-        this.contentGroup.add(text);
+        this.contentGroup.add(text, disabledAnchor);
     }
 
     public show() {
