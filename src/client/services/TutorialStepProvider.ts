195cce322ba095d3e55d59f0332253f1d984b9f6
generalustelaru <adrianpope@protonmail.com>
2026-03-10 13:54:15 +0200

    Advanced the tutorial.

--- src/client/services/TutorialStepProvider.ts
+++ src/client/services/TutorialStepProvider.ts
@@ -1,6 +1,7 @@
 import { PlayState, Action } from '~/shared_types';
 import { ScenarioStepPartial, Target } from '../client_types';
 
+const position = { x: 0, y: 0 } as const;
 export class TutorialStepProvider {
     private currentStep: number = 0;
     private partials: Array<ScenarioStepPartial> = [
@@ -16,7 +17,7 @@ export class TutorialStepProvider {
                 { highlights: [Target.bottomRightZone] },
                 { highlights: [Target.centerZone] },
             ],
-            expecting: { action: Action.reposition, payload: { position: { x: 0, y: 0 } } },
+            expecting: { action: Action.reposition, payload: { position } },
         },
         {
             transmission: null,
@@ -29,19 +30,19 @@ export class TutorialStepProvider {
                 { highlights: [Target.centerZone] },
                 { highlights: [Target.movesCounter, Target.topLeftZone] },
             ],
-            expecting: { action: Action.move, payload: { zoneId: 'topLeft', position: { x: 0, y: 0 } } },
+            expecting: { action: Action.move, payload: { zoneId: 'topLeft', position } },
         },
         {
             transmission: null,
             mutate: (state: PlayState) => {
-                const player = state.players[0];
-                player.bearings.seaZone = 'topLeft';
-                player.bearings.location = 'forest';
-                player.moveActions = 1;
-                player.mayUndo = true;
-                player.isAnchored = true;
-                player.locationActions = [Action.load_good];
-                player.destinations = ['left', 'topRight'];
+                const p = state.players[0];
+                p.bearings.seaZone = 'topLeft';
+                p.bearings.location = 'forest';
+                p.moveActions = 1;
+                p.mayUndo = true;
+                p.isAnchored = true;
+                p.locationActions = [Action.load_good];
+                p.destinations = ['left', 'topRight'];
             },
             visuals: [
                 { highlights: [Target.movesCounter] },
@@ -55,10 +56,10 @@ export class TutorialStepProvider {
         {
             transmission: null,
             mutate: (state: PlayState) => {
-                const player = state.players[0];
-                player.moveActions = 0;
-                player.cargo = ['gems', 'empty'];
-                player.locationActions = [];
+                const p = state.players[0];
+                p.moveActions = 0;
+                p.cargo = ['gems', 'empty'];
+                p.locationActions = [];
             },
             visuals: [
                 { highlights: [Target.cargoBand] },
@@ -71,26 +72,26 @@ export class TutorialStepProvider {
         {
             transmission: 'turnStart',
             mutate: (state: PlayState) => {
-                const player = state.players[0];
-                player.overnightZone = 'topLeft';
-                player.mayUndo = false;
-                player.isAnchored = false;
-                player.moveActions = 2;
-                player.destinations = ['left', 'center', 'topRight'];
+                const p = state.players[0];
+                p.overnightZone = 'topLeft';
+                p.mayUndo = false;
+                p.isAnchored = false;
+                p.moveActions = 2;
+                p.destinations = ['left', 'center', 'topRight'];
             },
             visuals: [
                 { highlights: [] },
                 { highlights: [Target.topRightZone] },
                 { highlights: [Target.rivalInfluence] },
             ],
-            expecting: { action: Action.move, payload: { zoneId: 'topRight', position: { x: 0, y: 0 } } },
+            expecting: { action: Action.move, payload: { zoneId: 'topRight', position } },
         },
         {
             transmission: 'failedMove',
             mutate: (state: PlayState) => {
-                const player = state.players[0];
-                player.influence = 2;
-                player.moveActions = 1;
+                const p = state.players[0];
+                p.influence = 2;
+                p.moveActions = 1;
 
                 if (state.rival.isIncluded)
                     state.rival.influence = 2;
@@ -100,24 +101,24 @@ export class TutorialStepProvider {
                 { highlights: [Target.rivalInfluence] },
                 { highlights: [Target.topRightZone] },
             ],
-            expecting: { action: Action.move, payload: { zoneId: 'topRight', position: { x: 0, y: 0 } } },
+            expecting: { action: Action.move, payload: { zoneId: 'topRight', position } },
         },
         {
             transmission: 'rivalControl',
             mutate: (state: PlayState) => {
-                const player = state.players[0];
-                player.influence = 6;
-                player.moveActions = 0;
-                player.bearings.seaZone = 'topRight';
-                player.bearings.location = 'market';
-                player.destinations = [];
-                player.isAnchored = true;
-                player.isHandlingRival = true;
+                const p = state.players[0];
+                p.influence = 6;
+                p.moveActions = 0;
+                p.bearings.seaZone = 'topRight';
+                p.bearings.location = 'market';
+                p.destinations = [];
+                p.isAnchored = true;
+                p.isHandlingRival = true;
 
                 if (state.rival.isIncluded) {
-                    const rival = state.rival;
-                    rival.activePlayerColor = 'Purple',
-                    rival.isControllable = true;
+                    const r = state.rival;
+                    r.activePlayerColor = 'Purple',
+                    r.isControllable = true;
                 }
             },
             visuals: [
@@ -126,7 +127,7 @@ export class TutorialStepProvider {
                 { highlights: [Target.rivalMoves] },
                 { highlights: [Target.rightZone] },
             ],
-            expecting: { action: Action.move_rival, payload: { zoneId: 'right', position: { x: 0, y: 0 } } },
+            expecting: { action: Action.move_rival, payload: { zoneId: 'right', position } },
         },
         {
             transmission: null,
@@ -134,11 +135,11 @@ export class TutorialStepProvider {
                 state.players[0].mayUndo = true;
 
                 if(state.rival.isIncluded) {
-                    const rival = state.rival;
-                    rival.bearings.seaZone = 'right';
-                    rival.bearings.location = 'treasury';
-                    rival.moves = 1;
-                    rival.destinations = ['bottomRight'];
+                    const r = state.rival;
+                    r.bearings.seaZone = 'right';
+                    r.bearings.location = 'treasury';
+                    r.moves = 1;
+                    r.destinations = ['bottomRight'];
                 }
             },
             visuals: [
@@ -160,6 +161,7 @@ export class TutorialStepProvider {
                 p.isHandlingRival = false;
                 p.locationActions = [Action.sell_goods, Action.sell_specialty];
                 p.feasibleTrades = [{ slot: 'slot_3', missing: [] }];
+                p.mayUndo = false;
             },
             visuals: [
                 { highlights: [Target.rivalInfluence] },
@@ -205,7 +207,161 @@ export class TutorialStepProvider {
         },
         {
             transmission: null,
-            mutate: (_state: PlayState) => { },
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.cargo = ['empty', 'empty'];
+                p.coins = 3;
+                p.feasibleTrades = [];
+                p.locationActions = [];
+                const m = state.market;
+                m.deckSize -= 1;
+                m.slot_3 = m.slot_2;
+                m.slot_2 = m.slot_1;
+                m.slot_1 = m.future;
+                m.future = { request: ['ebony','linen'], reward: { coins: 2, favorAndVp: 2 } };
+            },
+            visuals: [
+                { highlights: [Target.goldForCoin] },
+                { highlights: [Target.marketArea] },
+                { highlights: [Target.marketArea, Target.deck] },
+                { highlights: [Target.marketArea] },
+                { highlights: [Target.fluctuation_up, Target.fluctuation_down, Target.temple_mark] },
+                { highlights: [Target.fluctuation_up] },
+                { highlights: [Target.fluctuation_down] },
+                { highlights: [Target.fluctuation_up, Target.fluctuation_down] },
+                { highlights: [Target.temple_mark] },
+                { highlights: [Target.endTurnButton] },
+            ],
+            expecting: { action: Action.end_turn, payload: null },
+        },
+        {
+            transmission: 'turnStart',
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.isAnchored = false;
+                p.moveActions = 2;
+                p.destinations = ['right', 'topLeft'];
+            },
+            visuals: [
+                { highlights: [Target.rightZone, Target.goldForCoin, Target.silverForCoin] },
+                { highlights: [Target.rightZone, Target.rivalInfluence] },
+                { highlights: [Target.rivalInfluence] },
+                { highlights: [Target.favorDial, Target.cargoBand] },
+                { highlights: [Target.bottomRightZone, Target.templeArea] },
+                { highlights: [Target.topLeftZone] },
+            ],
+            expecting: { action: Action.move, payload: { zoneId: 'topLeft', position } },
+        },
+        {
+            transmission: null,
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.isAnchored = true;
+                p.moveActions = 1;
+                p.bearings.seaZone = 'topLeft';
+                p.bearings.location = 'mines';
+                p.destinations = ['left', 'center'];
+                p.mayUndo = true;
+                p.locationActions = [Action.load_good];
+            },
+            visuals: [
+                { highlights: [Target.marketCard] },
+                { highlights: [Target.topLeftZone] },
+            ],
+            expecting: { action: Action.load_good, payload: { tradeGood: 'gems', drop: null } },
+        },
+        {
+            transmission: null,
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.moveActions = 0;
+                p.cargo = ['gems', 'empty'];
+                p.mayUndo = true;
+                p.locationActions = [];
+            },
+            visuals: [
+                { highlights: [Target.endTurnButton] },
+            ],
+            expecting: { action: Action.end_turn, payload: null },
+        },
+        {
+            transmission: 'turnStart',
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.isAnchored = false;
+                p.mayUndo = false;
+                p.moveActions = 2;
+                p.destinations = ['left', 'center', 'topRight'];
+            },
+            visuals: [
+                { highlights: [Target.centerZone] },
+            ],
+            expecting: { action: Action.move, payload: { zoneId: 'center', position } },
+        },
+        {
+            transmission: null,
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.moveActions = 1;
+                p.bearings.seaZone = 'center';
+                p.bearings.location = 'farms';
+                p.destinations = ['left', 'bottomLeft', 'bottomRight'];
+                p.mayUndo = true;
+                p.isAnchored = true;
+                p.locationActions = [Action.load_good];
+            },
+            visuals: [
+                { highlights: [Target.centerZone] },
+            ],
+            expecting: { action: Action.load_good, payload: { tradeGood: 'linen', drop: null } },
+        },
+        {
+            transmission: null,
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.moveActions = 0;
+                p.locationActions = [];
+                p.cargo = ['gems', 'linen'];
+            },
+            visuals: [
+                { highlights: [Target.endTurnButton] },
+            ],
+            expecting: { action: Action.end_turn, payload: null },
+        },
+        {
+            transmission: 'turnStart',
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.mayUndo = false;
+                p.isAnchored = false;
+                p.moveActions = 2;
+                p.destinations = ['topLeft', 'left', 'bottomLeft', 'bottomRight'];
+            },
+            visuals: [
+                { highlights: [Target.bottomRightZone] },
+            ],
+            expecting: { action: Action.move, payload: { zoneId: 'bottomRight', position } },
+        },
+        {
+            transmission: null,
+            mutate: (state: PlayState) => {
+                const p = state.players[0];
+                p.mayUndo = true;
+                p.isAnchored = true;
+                p.moveActions = 1;
+                p.bearings.seaZone = 'bottomRight';
+                p.bearings.location = 'temple';
+                p.destinations = ['bottomLeft', 'right'];
+                p.locationActions = [Action.donate_goods, Action.upgrade_cargo];
+            },
+            visuals: [
+                { highlights: [] },
+            ],
+            expecting: null,
+        },
+        { // TEMPLATE!
+            transmission: null,
+            mutate: (_state: PlayState) => {},
             visuals: [
                 { highlights: [] },
             ],
